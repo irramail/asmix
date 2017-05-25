1c814c47f0f05ae12c5ae315c92870fbaf18fd4b
Roman <cerebrum.ch@gmail.com>
2017-05-25 16:58:08 +0700

    add serch on massege

--- app/models/plsbgvideo.rb
+++ app/models/plsbgvideo.rb
@@ -6,4 +6,12 @@ class Plsbgvideo < ActiveRecord::Base
   has_many :mediafiles, :through => :plsbgvideo_mediafiles
 
   default_scope { order("created_at DESC") }
+
+  def self.search(search)
+    if search
+      where('name LIKE or id LIKE ?', "%#{search}%" "%#{search}%")
+    else
+      all
+    end
+  end
 end
